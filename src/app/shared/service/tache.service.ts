3be466cf4a801ee9103c8f9346a9dae4bc08ac24
EYA MANAI <eyamanai35@outlook.com>
2024-07-21 23:41:48 +0200

    Creating taches components & Using tache service

---
+++ src/app/shared/service/tache.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Tache } from '../interfaces/tache';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TacheService {
+  taches: Tache[] = [];
+  constructor() {}
+  getTaches() {
+    return this.taches;
+  }
+
+  addTache(tache: Tache) {
+    this.taches.push(tache);
+    console.table(this.taches);
+  }
+
+  deleteTache(id: number) {
+    this.taches.splice(id, 1);
+  }
+}
